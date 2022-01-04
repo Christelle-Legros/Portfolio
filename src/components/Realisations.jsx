6b46ded91b938e7ca53caca7a6f237738152e262
Christelle LEGROS <christelle.legros80@sfr.fr>
2022-01-04 10:09:28 +0100

    Modif responsive

--- src/components/Realisations.jsx
+++ src/components/Realisations.jsx
@@ -8,37 +8,37 @@ const Realisations = () => {
     <div className="realisations" id="realisations">
       <p className="realisations__title">Mes réalisations</p>
       <div className="realisations__projectsContainer">
-        <a
-          href="https://christelle-legros.github.io/WorldWildQuiz/#/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="realisations__projects" id="realisations__wwqProject">
+        <div className="realisations__projects" id="realisations__wwqProject">
+          <a
+            href="https://christelle-legros.github.io/WorldWildQuiz/#/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img src={WwqVisuel} alt="Screenshot World Wild Quiz" />
 
             <p>
               Projet 2 Wild Code School : création d'un quiz et d'un memory avec
               ReactJs en faisant appel à une API
             </p>
-          </div>
-        </a>
-        <a
-          href="https://christelle-legros.github.io/Liveup/#/"
-          target="_blank"
-          rel="noreferrer"
+          </a>
+        </div>
+
+        <div
+          className="realisations__projects"
+          id="realisations__liveUpProject"
         >
-          <div
-            className="realisations__projects"
-            id="realisations__liveUpProject"
+          <a
+            href="https://christelle-legros.github.io/Liveup/#/"
+            target="_blank"
+            rel="noreferrer"
           >
             <img src={LiveUpVisuel} alt="Screenshot Live Up" />
             <p>
-              Hackaton d'environ 30 heures : création d'un site fullstack avec
-              utilisation de ReactJs pour le front, Express pour le back et
-              MySql pour la base de données.
+              Hackaton 30 heures environ : création d'un site fullstack avec :
+              ReactJs pour le front, Express pour le back et MySql pour la bdd.
             </p>
-          </div>
-        </a>
+          </a>
+        </div>
 
         <div className="realisations__projects" id="luneticProject">
           <img src={LuneticVisuel} alt="Screenshot Lunetic" />
