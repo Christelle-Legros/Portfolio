4a6e1003221f033a094ee7f06d358dd8d857ef97
Christelle LEGROS <christelle.legros80@sfr.fr>
2022-06-16 16:05:34 +0200

    Add background video

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import logoCL from "../assets/logo.png";
+import videoBackground from "../assets/UrsuiaLever.mp4";
 
 const Home = () => {
   return (
     <div className="home" id="home">
-      <div className="home__logo">
-        <img src={logoCL} alt="Logo CL" />
+      <div className="home__backvideo">
+        <video autoPlay loop muted>
+          <source src={videoBackground} type="video/mp4" />
+        </video>
       </div>
+      <img src={logoCL} alt="Logo CL" />
     </div>
   );
 };
